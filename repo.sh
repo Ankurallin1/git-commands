be45e50ffd8d579d8bd84726fb9315dcba58c67f
Ankur srivastava <ankursrivastava2070@gmail.com>
2022-10-08 16:27:06 +0530

    script to create repo using CLI

---
+++ repo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+git config --global user.name "Ankur Srivastava"
+git config --global user.email "201b049@juetguna.in"
+echo "Enter reponame"
+read reponame
+#mkdir ${reponame}
+#cd `dirname $(type -p reponame)`
+curl -i -H "Authorization: token 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" -d '{"name": "'$reponame'","private": true>git checkout -b main
+echo "Enter commit mssg"
+read msg
+touch README.md
+git add README.md
+git commit -m "$msg"
+git remote add origin https://github.com/Unknown-9015/$reponame.git
+echo "paste access token as password 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"
+git push origin main
